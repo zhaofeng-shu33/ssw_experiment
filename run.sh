ff4f6f6d7c4fbf3eae682b54dcc86f3be0b9351b
zhaofeng-shu33 <616545598@qq.com>
2019-10-21 16:49:02 +0800

    add compile

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e -x
 mkdir -p originalData
-if [ ! -f originalData/uniprot_humanProteinList.fasta ]; then
-  cd originalData
+cd originalData
+if [ ! -f uniprot_humanProteinList.fasta ]; then
   wget "https://www.uniprot.org/uniprot/?query=reviewed:yes%20AND%20proteome:up000005640&format=fasta&force=true&compress=yes" -O uniprot_humanProteinList.fasta.gz
   gunzip uniprot_humanProteinList.fasta.gz
 fi
 cd ../lambdaPackage
 mkdir -p proteinPartitions
 python partitionProteins.py 
+cd ../ssw
+make
+cp ssw_test ../lambdaPackage/
+cd ../lambdaPackage
+
+
