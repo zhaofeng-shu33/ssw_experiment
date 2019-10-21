26b60e8d33f61f7b9633667077ee4e4b86ed08b7
zhaofeng-shu33 <616545598@qq.com>
2019-10-21 16:29:29 +0800

    add pipelines

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e -x
+mkdir -p originalData
+if [ ! -f originalData/uniprot_humanProteinList.fasta ]; then
+  cd originalData
+  wget "https://www.uniprot.org/uniprot/?query=reviewed:yes%20AND%20proteome:up000005640&format=fasta&force=true&compress=yes" -O uniprot_humanProteinList.fasta.gz
+  gunzip uniprot_humanProteinList.fasta.gz
+fi
+cd ../lambdaPackage
+python partitionProteins.py 
