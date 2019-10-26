2e9862089f8fb5c83c4ec5d98603b11575f28482
zhaofeng-shu33 <616545598@qq.com>
2019-10-26 23:46:26 +0800

    fix import os problem

--- client/client_config.py
+++ client/client_config.py
@@ -1,3 +1,5 @@
+import os
+
 lambdaName = r"arn:aws:lambda:us-east-2:369401945610:function:ssw" #The ARN of the AWS Lambda function
 sqsQueueUrl = r"https://sqs.us-east-2.amazonaws.com/369401945610/ssw" #The URL of the AWS SQS Queue
 s3ResultsBucket = r"ssw-azure-pipelines" #The bucket name of the AWS S3 Bucket
