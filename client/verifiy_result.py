6d7f2123f7de64898def067b07ce093bd44a8ded
zhaofeng-shu33 <616545598@qq.com>
2019-10-27 14:49:29 +0800

    add verify step

---
+++ client/verifiy_result.py
@@ -0,0 +1,45 @@
+from filecmp import dircmp
+import boto3
+import pathlib
+
+def downloadResults(s3Bucket, resultsPath):
+    pathlib.Path(resultsPath).mkdir(parents=True, exist_ok=True)
+    my_bucket = boto3.resource('s3').Bucket(s3Bucket)
+
+    for object in my_bucket.objects.all():
+        my_bucket.download_file(object.key, resultsPath + object.key)
+
+def areFoldersDifferent(dir1, dir2):
+    dcmp = dircmp(dir1, dir2)
+    differences = len(dcmp.diff_files) + len(dcmp.left_only) + len(dcmp.right_only)
+    return differences > 0
+
+def verifyResults(basisResultsPath, trialResultsPath, metricsPath):
+    with open(metricsPath + "verification.txt", "w") as output_f:
+        if areFoldersDifferent(basisResultsPath, trialResultsPath):
+            output_f.write("Basis results at [" + basisResultsPath + "] were DIFFERENT (INCORRECT) from trial results in [" + trialResultsPath + "]\n")
+        else:
+            output_f.write("Basis results at [" + basisResultsPath + "] were IDENTICAL (CORRECT) to trial results in [" + trialResultsPath + "]\n")
+
+def cleanup(s3Bucket, queueUrl):
+    my_bucket = boto3.resource('s3').Bucket(s3Bucket)
+    s3Client = boto3.client("s3")
+    for object in my_bucket.objects.all():
+        s3Client.delete_object(
+            Bucket=s3Bucket,
+            Key=object.key
+        )
+    sqsClient = boto3.client("sqs")
+    sqsClient.purge_queue(
+        QueueUrl=queueUrl
+    )
+
+if __name__ == '__main__':
+    concurrencyLimit = 1000
+    trialNumber = 1
+    path = r"./performanceData/concurrency" + str(concurrencyLimit) + "/trial" + str(trialNumber) + "/"
+    trialResultsDir = path + "results/"
+    basisResultsDir = "./results_basis/"
+    downloadResults(s3ResultsBucket, trialResultsDir)
+    verifyResults(basisResultsDir, trialResultsDir, path)
+    cleanup(s3ResultsBucket, sqsQueueUrl)
