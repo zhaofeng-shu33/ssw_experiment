30ff3b142e211502138674f23e5a3cebf4c8f4e1
zhaofeng-shu33 <616545598@qq.com>
2019-10-27 15:45:13 +0800

    specify a region

--- .circleci/verify.sh
+++ .circleci/verify.sh
@@ -4,4 +4,5 @@ cd client
 wget https://gitlab.com/zhaofeng-shu33/ssw_experiment/-/jobs/333800685/artifacts/download -O artifacts.zip
 unzip artifacts.zip
 python verify_result.py
+export AWS_DEFAULT_REGION=us-east-2
 cat verification.txt
